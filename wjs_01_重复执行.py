2285699699a4a88c5ef137db910fb2f4f142641a
JasonMuscles <wangjinsenufo@qq.com>
2019-11-14 00:40:54 +0800

    3

---
+++ wjs_01_重复执行.py
@@ -0,0 +1,7 @@
+# 打印5遍 hello Python
+
+print("Hello Python")
+print("Hello Python")
+print("Hello Python")
+print("Hello Python")
+print("Hello Python")
---
+++ wjs_03_程序计数.py
@@ -0,0 +1,12 @@
+# 打印5遍 hello Python
+# 1.定义一个整数变量，记录循环次数
+i = 0
+
+# 2.开始循环
+while i < 5:
+
+    # 循环内执行的代码
+    print("Hello Python")
+    # 计数器
+    # i = i + 1
+    i += 1
